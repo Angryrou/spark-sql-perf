429f94a238d5a9896c8a0a3b07d1399d3b451c14
angryrou <chenghao@cs.umass.edu>
2022-06-10 14:37:25 +0200

    Create MyRunBenchmark.scala

---
+++ src/main/scala/com/databricks/spark/sql/perf/MyRunBenchmark.scala
@@ -0,0 +1,97 @@
+package com.databricks.spark.sql.perf
+
+import com.databricks.spark.sql.perf.tpch.TPCH
+import com.databricks.spark.sql.perf.ExecutionMode.CollectResults
+import com.databricks.spark.sql.perf.tpcds.TPCDS
+import org.apache.commons.io.IOUtils
+import org.apache.spark.sql.SparkSession
+
+
+case class RunBenchmarkConfig
+(
+  benchmarkName: String = null, // TPCH / TPCDS
+  dataGenDir: String = "/mnt/disk7/chenghao-dataset",
+  scaleFactor: String = null, // 1
+  locationHeader: String = "hdfs://node13-opa:8020/user/spark_benchmark",
+  overwrite: Boolean = false,
+)
+
+object MyRunBenchmark {
+
+  val iterations = 1
+  val timeout = 24*60*60 // timeout, in seconds.
+
+  def main(args: Array[String]): Unit = {
+    val parser = new scopt.OptionParser[RunBenchmarkConfig]("Gen-Benchmark-data") {
+      opt[String]('b', "benchmark")
+        .action { (x, c) => c.copy(benchmarkName = x) }
+        .text("the name of the benchmark to run")
+        .required()
+      opt[String]('d', "dataGenDir")
+        .action { (x, c) => c.copy(dataGenDir = x) }
+        .text("head dir of dsdgen/dbgen")
+        .required()
+      opt[String]('s', "scaleFactor")
+        .action((x, c) => c.copy(scaleFactor = x))
+        .text("scaleFactor defines the size of the dataset to generate (in GB)")
+      opt[String]('l', "locationHeader")
+        .action((x, c) => c.copy(locationHeader = x))
+        .text("head root directory of location to create data in")
+      opt[Boolean]('o', "overwrite")
+        .action((x, c) => c.copy(overwrite = x))
+        .text("overwrite the data that is already there")
+      help("help")
+        .text("prints this usage text")
+    }
+
+    parser.parse(args, RunBenchmarkConfig()) match {
+      case Some(config) =>
+        run(config)
+      case None =>
+        System.exit(1)
+    }
+  }
+
+  def run(config: RunBenchmarkConfig): Unit = {
+    assert(config.benchmarkName == "TPCH" || config.benchmarkName == "TPCDS")
+
+    val spark = SparkSession
+      .builder()
+      .enableHiveSupport()
+      .getOrCreate()
+    val sc = spark.sparkContext
+
+    val resultLocation = s"${config.locationHeader}/${config.benchmarkName.toLowerCase}_sf${config.scaleFactor}/results" // place to write results
+    val databaseName = s"${config.benchmarkName.toLowerCase}_${config.scaleFactor}" // name of database to create.
+
+    spark.sql(s"use $databaseName")
+
+    if (config.benchmarkName == "TPCDS") {
+      val tpcds = new TPCDS (sqlContext = spark.sqlContext)
+      val queries = tpcds.tpcds2_4Queries
+      val experiment = tpcds.runExperiment(
+        queries,
+        iterations = iterations,
+        resultLocation = resultLocation,
+        tags = Map("database" -> databaseName, "scale_factor" -> config.scaleFactor)
+      )
+      experiment.waitForFinish(timeout)
+    } else if (config.benchmarkName == "TPCH") {
+      val tpch = new TPCH(sqlContext = spark.sqlContext)
+      val queries = (1 to 22).map { q =>
+        val queryContent: String = IOUtils.toString(
+          getClass().getClassLoader().getResourceAsStream(s"tpch/queries/$q.sql"))
+        new Query(s"Q$q", spark.sqlContext.sql(queryContent), description = s"TPCH Query $q",
+          executionMode = CollectResults)
+      }
+      val experiment = tpch.runExperiment(
+        queries,
+        iterations = iterations,
+        resultLocation = resultLocation,
+        tags=Map("database" -> databaseName, "scale_factor" -> config.scaleFactor))
+      experiment.waitForFinish(timeout)
+    } else {
+      throw new IllegalArgumentException(s"${config.benchmarkName} is not supported.")
+    }
+  }
+}
