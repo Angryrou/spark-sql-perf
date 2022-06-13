d3c175be3aadfb4b04ff60dadee63e288c98fbd8
angryrou <chenghao@cs.umass.edu>
2022-06-13 19:26:19 +0200

    update

--- src/main/scripts/my_set_benchmark.sh
+++ src/main/scripts/my_set_benchmark.sh
@@ -15,12 +15,13 @@ lpath=/opt/hex_users/$USER/chenghao/spark-sql-perf/src/main/resources/log4j.prop
 --conf spark.executor.cores=4 \
 --conf spark.executor.memory=32g \
 --conf spark.reducer.maxSizeInFlight=256m \
+--conf spark.rpc.askTimeout=12000
 --driver-java-options "-Dlog4j.configuration=file:$lpath" \
 --conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=file:log4j.properties" \
 --files "$lpath" \
 --jars ~/spark/examples/jars/scopt_2.12-3.7.1.jar \
 /opt/hex_users/$USER/chenghao/spark-sql-perf/target/scala-2.12/spark-sql-perf_2.12-0.5.1-SNAPSHOT.jar \
--b $bm -d /mnt/disk7/chenghao-dataset -s $sf -l hdfs://${HOSTNAME}-opa:8020/user/spark_benchmark -n new_${bm}_${sf}
+-b $bm -d /mnt/disk7/chenghao-dataset -s $sf -l hdfs://${HOSTNAME}-opa:8020/user/spark_benchmark -n new_${bm}_${sf} -o true
 
 #bash ~/chenghao/spark-sql-perf/src/main/scripts/my_set_benchmark.sh TPCH 10
 #bash ~/chenghao/spark-sql-perf/src/main/scripts/my_set_benchmark.sh TPCH 100
